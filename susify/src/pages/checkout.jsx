6d05c8e11c22f601cd81fb77e033bd70df708276
AdonisZK <adonisvefleyor@gmail.com>
2023-06-10 17:11:46 +0700

    7:00:00

--- susify/src/pages/checkout.jsx
+++ susify/src/pages/checkout.jsx
@@ -3,11 +3,17 @@ import axios from "axios";
 import { CREATE_ORDER } from "../utils/constants";
 import { useRouter } from "next/router";
 import { useCookies } from "react-cookie";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import CheckoutForm from "@/components/CheckoutForm";
+
 
 function checkout() {
   const [clientSecret, setClientSecret] = useState("");
   const [cookies, setCookies] = useCookies();
   const router = useRouter();
+  const stripePromise = loadStripe("pk_test_51NHNCKJB6wzGHIxru8vXuNaXslutNr3alJU4wcWP1eLxG6ZZRDjTOeoaIkHWpoQigiMmXJ3rUaA1MpM7AUzQV09S00bGBsaV2J");
+
   const { listingId } = router.query;
 
   useEffect(() => {
@@ -32,7 +38,26 @@ function checkout() {
     if (listingId) createOrder();
   }, [listingId]);
 
-  return <div> checkout </div>;
+  const appearance = {
+    theme: "stripe",
+  };
+  const options = {
+    clientSecret,
+    appearance,
+  };
+
+  return (
+    <div className="min-h-[80vh] max-w-full mx-20 flex flex-col gap-10 items-center">
+      <h1 className="text-3xl">
+        Please complete the payment to place the order.
+      </h1>
+      {clientSecret && (
+        <Elements options={options} stripe={stripePromise}>
+          <CheckoutForm />
+        </Elements>
+      )}
+    </div>
+  );
 }
 
 export default checkout;
